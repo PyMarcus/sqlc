4117fee4870c232fc14bee39b83d208017c3a7e7
Marcus <marcus-v@outlook.com>
2024-07-12 20:21:04 -0300

    added middleware  test

---
+++ api/middleware.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/PyMarcus/go_sqlc/token"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+	authorizationPayloadKey = "authorization_payload_key"
+)
+
+func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader(authorizationHeaderKey)
+		if len(authHeader) == 0 {
+			err := errors.New("authorization header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(authHeader)
+		if len(fields) < 2 {
+			err := errors.New("invalid authorization header has been provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		authType := strings.ToLower(fields[0])
+		if authType != authorizationTypeBearer {
+			err := errors.New("unsupported authorization type")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		accessToken := fields[1]
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+		ctx.Set(authorizationPayloadKey, payload)
+		ctx.Next()
+	}
+}
